bd9f78431e67e46bcf8bcaa0ad995eb036b04ba8
Ken M. Haggerty <kenmhaggerty@protonmail.com>
2019-05-10 20:13:57 -0500

    Add new Xcode watchOS project

---
+++ KMHTimeTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KMHTimeTracker
+//
+//  Created by Ken M. Haggerty on 5/10/19.
+//  Copyright © 2019 Ken M. Haggerty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
